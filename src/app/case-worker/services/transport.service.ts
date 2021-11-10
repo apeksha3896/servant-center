c5051be35233ac2ad8a47d958b904ac2503eb84a
apeksha3896 <Apeksha.Horambe@mindtree.com>
2021-11-10 11:36:14 +0530

    commited with removed files

--- src/app/case-worker/services/transport.service.ts
+++ src/app/case-worker/services/transport.service.ts
@@ -2,19 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TransportService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTransportRequestFormData() {
     return this.http.get('./assets/mock/transportdata.json');
   }
-
-  // getTableData() {
-  //     return this.http.get('./assets/mock/tableValues.json');
-  //     // let url="https://jsonplaceholder.typicode.com/todos/";
-  //     // return this.http.get(url);
-  //   }
 }
